3a61f25ccd6952c624c5b35cdce8b502d8535c2b
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-04-27 19:07:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-import setuptools
+try:
+    import setuptools
+except ImportError:
+    print("Can't import SetupTools from STDLib. Please install it first!")
+"""
+    ZypeLang - A OpenSource, easy-to-use, easy-to-read & easy-to-write replacement of JSON based on Python.
+"""
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+    
+
 setuptools.setup(
     name="ZypeSDK",
     version="1.1",
